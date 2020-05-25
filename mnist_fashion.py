bdb30d3dfcdc944e540a44b6df20718b413c7e83
pinachibajaj <pinachibajaj@gmail.com>
2020-05-25 19:04:14 +0530

    first

---
+++ mnist_fashion.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import struct
+import numpy as np
+
+def read_idx(filename):
+    """Credit: https://gist.github.com/tylerneylon"""
+    with open(filename, 'rb') as f:
+        zero, data_type, dims = struct.unpack('>HBB', f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
+
+
+# In[3]:
+
+
+x_train = read_idx("train-images-idx3-ubyte")
+y_train = read_idx("train-labels-idx1-ubyte")
+x_test = read_idx("t10k-images-idx3-ubyte")
+y_test = read_idx("t10k-labels-idx1-ubyte")
+
+
+# In[4]:
+
+
+import matplotlib.pyplot as plt
+
+plt.subplot(331)
+random_num = np.random.randint(0,len(x_train))
+plt.imshow(x_train[random_num], cmap=plt.get_cmap('gray'))
+
+plt.subplot(332)
+random_num = np.random.randint(0,len(x_train))
+plt.imshow(x_train[random_num], cmap=plt.get_cmap('gray'))
+
+plt.subplot(333)
+random_num = np.random.randint(0,len(x_train))
+plt.imshow(x_train[random_num], cmap=plt.get_cmap('gray'))
+
+plt.subplot(334)
+random_num = np.random.randint(0,len(x_train))
+plt.imshow(x_train[random_num], cmap=plt.get_cmap('gray'))
+
+plt.subplot(335)
+random_num = np.random.randint(0,len(x_train))
+plt.imshow(x_train[random_num], cmap=plt.get_cmap('gray'))
+
+plt.subplot(336)
+random_num = np.random.randint(0,len(x_train))
+plt.imshow(x_train[random_num], cmap=plt.get_cmap('gray'))
+
+plt.show()
+
+
+# In[7]:
+
+
+from keras.datasets import mnist
+from keras.utils import np_utils
+import keras
+from keras.models import Sequential
+from keras.layers import Dense , Dropout , Flatten
+from keras.layers import Conv2D , MaxPooling2D , BatchNormalization
+
+
+# In[8]:
+
+
+batch_size = 128
+epochs = 3
+
+
+# In[15]:
+
+
+img_rows = x_train[0].shape[0]
+img_col = x_train[0].shape[1]
+
+
+# In[24]:
+
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_col, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_col, 1)
+
+input_shape = (img_rows , img_col , 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train /= 255
+x_test /= 255
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_test.shape[1]
+num_pixels = x_train.shape[1] * x_train.shape[2]
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(BatchNormalization())
+
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = keras.optimizers.Adadelta(),
+              metrics = ['accuracy'])
+
+print(model.summary())
+
+
+# In[26]:
+
+
+history = model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
