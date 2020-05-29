d0bf6904f48aa05d87a279052f89f37965dea255
pinachibajaj <pinachibajaj@gmail.com>
2020-05-29 22:07:12 +0530

    first

--- mnist_fashion.py
+++ mnist_fashion.py
@@ -9,13 +9,42 @@ def read_idx(filename):
         shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
         return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
 
+
+# In[18]:
+
+
 x_train = read_idx("train-images-idx3-ubyte")
 y_train = read_idx("train-labels-idx1-ubyte")
 x_test = read_idx("t10k-images-idx3-ubyte")
 y_test = read_idx("t10k-labels-idx1-ubyte")
 
+
+# In[19]:
+
+
+
+print("Initial shape or dimensions of x_train", str(x_train.shape))
+
+print ("Number of samples in our training data: " + str(len(x_train)))
+print ("Number of labels in our training data: " + str(len(y_train)))
+print ("Number of samples in our test data: " + str(len(x_test)))
+print ("Number of labels in our test data: " + str(len(y_test)))
+print()
+print ("Dimensions of x_train:" + str(x_train[0].shape))
+print ("Labels in x_train:" + str(y_train.shape))
+print()
+print ("Dimensions of x_test:" + str(x_test[0].shape))
+print ("Labels in y_test:" + str(y_test.shape))
+
+
+# In[20]:
+
+
+# Let's do the same thing but using matplotlib to plot 6 images 
 import matplotlib.pyplot as plt
 
+# Plots 6 images, note subplot's arugments are nrows,ncols,index
+# we set the color map to grey since our image dataset is grayscale
 plt.subplot(331)
 random_num = np.random.randint(0,len(x_train))
 plt.imshow(x_train[random_num], cmap=plt.get_cmap('gray'))
@@ -40,25 +69,34 @@ plt.subplot(336)
 random_num = np.random.randint(0,len(x_train))
 plt.imshow(x_train[random_num], cmap=plt.get_cmap('gray'))
 
+# Display out plots
 plt.show()
 
-from keras.datasets import mnist
-from keras.utils import np_utils
+
+# In[21]:
+
+
 import keras
+from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense , Dropout , Flatten
-from keras.layers import Conv2D , MaxPooling2D , BatchNormalization
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras import backend as K
+from keras.utils import np_utils
 
-batch_size = 128
-epochs = 3
 
+# In[22]:
+
+
+# Lets store the number of rows and columns
 img_rows = x_train[0].shape[0]
-img_col = x_train[0].shape[1]
+img_cols = x_train[1].shape[0]
 
-x_train = x_train.reshape(x_train.shape[0], img_rows, img_col, 1)
-x_test = x_test.reshape(x_test.shape[0], img_rows, img_col, 1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 
-input_shape = (img_rows , img_col , 1)
+# store the shape of a single image 
+input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -66,12 +104,21 @@ x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# Now we one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
+
+# Let's count the number columns in our hot encoded matrix 
+print ("Number of Classes: " + str(y_test.shape[1]))
 
 num_classes = y_test.shape[1]
 num_pixels = x_train.shape[1] * x_train.shape[2]
 
+# create model
 model = Sequential()
 
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -98,17 +145,51 @@ model.compile(loss = 'categorical_crossentropy',
 
 print(model.summary())
 
-history = model.fit(x_train, y_train,
+
+# In[24]:
+
+
+f = open("epochs.txt","r")
+epochs = f.read()
+
+
+# In[25]:
+
+
+epochs
+
+
+# In[26]:
+
+
+epochs=int(epochs)
+
+
+# In[27]:
+
+
+batch_size = 128
+
+
+# In[29]:
+
+
+def trained(epochs , batch_size):
+    history = model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(x_test, y_test))
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+    score = model.evaluate(x_test, y_test, verbose=0)
+    test_loss = score[0]
+    test_accuracy = score[1]
+    
+    with open("accuracy.txt","w") as f:
+        f.write("%.2f"%test_accuracy)
 
-loss = score[0]
-acc = score[1]
-with open("acc.txt" , "w") as f:
-    f.write("%.2f"%acc)
+
+# In[30]:
+
+
+trained(epochs,batch_size)
